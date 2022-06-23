94ad15ec1dd12439566b3486cd17bb9a25266da0
Thoai Dinh Thien <dtthoai@tma.com.vn>
2022-06-23 17:31:02 +0700

    thoai update

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,26 +1,22 @@
-import * as React from "react";
-import Link from "next/link";
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import React from 'react'
+import styles from "../styles/Home.module.css";
+import Image from "next/image";
 
 export default function Footer() {
   return (
-    <>
+      <>
       <footer className={styles.footer}>
-        <a
-          href="#"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={18} />
-          </span>
-        </a>
-      </footer>
-    </>
-  );
-}                                                                                                                                                                    
-
-
-
+				<a
+					href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Powered by{" "}
+					<span className={styles.logo}>
+						<Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+					</span>
+				</a>
+			</footer>
+      </>
+  )
+}
